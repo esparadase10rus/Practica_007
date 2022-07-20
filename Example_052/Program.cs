7d24526b6ed9c5f4d80cfd69943913a2a4f58b2f
Ilya <ackulin.ilya@yandex.ru>
2022-07-20 20:02:49 +0300

    Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.

---
+++ Example_052/Program.cs
@@ -0,0 +1,45 @@
+﻿Random random = new Random();
+int m = random.Next(2,16);
+int n = random.Next(2,16);
+
+ArithmeticMeanOfColumns(m, n, FillArray(m, n)); 
+Console.ForegroundColor = ConsoleColor.White; Console.WriteLine(""); Console.ReadLine();
+
+static int[,] FillArray(int m, int n)
+{
+    int[,] arr = new int[m, n];
+    Random random = new Random();
+    Console.WriteLine();
+    Console.WriteLine("Двумерный массив из целых чисел: "); Console.WriteLine(); Console.ForegroundColor = ConsoleColor.Yellow;
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = random.Next(-99, 100);
+            Console.Write("{0, 10}", arr[i, j]);
+        }
+        Console.WriteLine();Console.WriteLine();
+    }
+    Console.ForegroundColor = ConsoleColor.White;
+    return arr;
+}
+
+void ArithmeticMeanOfColumns(int m, int n, int[,] array)
+{
+Console.WriteLine(new string('_',10*n)); Console.WriteLine(""); 
+Console.WriteLine("Среднее арифметическое элементов в каждом столбце: "); 
+Console.WriteLine(""); Console.ForegroundColor = ConsoleColor.Yellow;
+
+for (int i = 0; i < n; i++)
+    {
+        double arc = 0;
+        for (int j = 0; j < m; j++)
+        {
+            arc += array[j, i];
+        }
+        arc = arc/2;
+        Console.Write("{0, 10}", (arc).ToString("0.0"));
+    }
+    Console.ForegroundColor = ConsoleColor.White;
+}
